9cefe060444ac30bd0eec4f214a9398371252ed6
Denis Celcer <denis.celcer@inova.si>
2019-02-13 09:50:16 +0100

    Init commit.

---
+++ app/src/main/java/com/example/zimskasolabeacons/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.zimskasolabeacons
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), BeaconScanner.Listener {
+
+    private var scanner = BeaconScanner(this, this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        scanner.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        scanner.stop()
+    }
+
+    override fun onBeaconFound(data: String) {
+        tvBeaconAttachment.text = data
+    }
+
+    override fun onBeaconLost(data: String) {
+        tvBeaconAttachment.setText(R.string.unknown_location)
+    }
+
+}
+
+
